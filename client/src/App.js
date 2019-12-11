e88bd89f4be53f4ee66d0f39288531cddea5e497
Miko Aro <arom@us.ibm.com@mikos-mbp.attlocal.net>
2019-12-11 12:25:50 -0600

    doing it live

--- client/src/App.js
+++ client/src/App.js
@@ -1,181 +1,125 @@
-import React, { Component } from "react";
-import { Button, Typography, Grid, TextField } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/styles";
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import axios from 'axios';
+var NumberFormat = require('react-number-format');
 
-import MyContract from "./contracts/MyContract.json";
-import getWeb3 from "./utils/getWeb3";
 
-import { theme } from "./utils/theme";
-import Header from "./components/Header";
-import "./App.css";
 
-const GAS = 500000;
-const GAS_PRICE = "20000000000";
 
-class App extends Component {
-  state = {
-    web3: null,
-    accounts: null,
-    contract: null,
-    randomLow: 1,
-    randomHigh: 6,
-    resultReceived: false,
-    result: "0"
+const AnotherPage = () => <h1>Another Page</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: [],
+      response: '',
+      post: '',
+      responseToPost: '',
+    };
+  }
+
+  componentDidMount() {
+      // Make a request for a user with a given ID
+      axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+        .then(res => {
+          // handle success
+          const cryptos = res.data;
+          console.log(cryptos);
+          this.setState({cryptos: cryptos});
+        })
+        .catch(error => {
+          console.log(error);
+        })
+
+        this.callApi()
+          .then(res => this.setState({ response: res.express }))
+          .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
   };
 
-  componentDidMount = async () => {
-    try {
-      const web3 = await getWeb3();
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
 
-      const accounts = await web3.eth.getAccounts();
-
-      const networkId = await web3.eth.net.getId();
-      if (networkId !== 3) {
-        throw new Error("Select the Ropsten network from your MetaMask plugin");
-      }
-      const deployedNetwork = MyContract.networks[networkId];
-      const contract = new web3.eth.Contract(
-        MyContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
-      this.setState({ web3, accounts, contract });
-
-      window.ethereum.on("accountsChanged", async accounts => {
-        const newAccounts = await web3.eth.getAccounts();
-        this.setState({ accounts: newAccounts });
-      });
-
-      // Refresh on-chain data every 1 second
-      const component = this;
-      async function loopRefresh() {
-        await component.refreshState();
-        setTimeout(loopRefresh, 1000);
-      }
-      loopRefresh();
-    } catch (error) {
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
-    }
-  };
-
-  refreshState = async () => {
-    const resultReceived = await this.state.contract.methods
-      .resultReceived()
-      .call();
-    const result = (
-      await this.state.contract.methods.result().call()
-    ).toString();
-    this.setState({ resultReceived, result });
-  };
-
-  handleUpdateForm = (name, value) => {
-    this.setState({ [name]: value });
-  };
-
-  handleRequestResult = async () => {
-    await this.state.contract.methods
-      .makeRequest(this.state.randomLow.toString(), this.state.randomHigh.toString())
-      .send({ from: this.state.accounts[0], gas: GAS, gasPrice: GAS_PRICE });
-  };
-
-  handleResetResult = async () => {
-    await this.state.contract.methods
-      .resetResult()
-      .send({ from: this.state.accounts[0], gas: GAS, gasPrice: GAS_PRICE });
+    this.setState({ responseToPost: body });
   };
 
   render() {
-    if (!this.state.web3) {
-      return (
-        <ThemeProvider theme={theme}>
-          <div className="App">
-            <Header />
-
-            <Typography>Loading Web3, accounts, and contract...</Typography>
-          </div>
-        </ThemeProvider>
-      );
-    }
     return (
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <Header />
+      <div className="App">
+      <div>
+        {Object.keys(this.state.cryptos).map((key) => (
 
-          <Typography variant="h5" style={{ marginTop: 32 }}>
-            {`Oracle is going to return an integer between ${this.state.randomLow} and ${this.state.randomHigh}`}
-          </Typography>
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+            <span className="right"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></span>
+          </div>
 
-          <Grid container style={{ marginTop: 32 }}>
-            <Grid item xs>
-              <Typography variant="h5" style={{ marginTop: 32 }}>
-                Lower limit
-              </Typography>
-            </Grid>
-            <Grid item xs>
-              <Typography variant="h5" style={{ marginTop: 32 }}>
-                Higher limit
-              </Typography>
-            </Grid>
-          </Grid>
-
-          <Grid container style={{ marginTop: 32 }}>
-            <Grid item xs>
-              <TextField
-                id="bet-amount"
-                className="input"
-                value={this.state.randomLow}
-                onChange={e =>
-                  this.handleUpdateForm("randomLow", e.target.value)
-                }
-              />
-            </Grid>
-            <Grid item xs>
-              <TextField
-                id="bet-amount"
-                className="input"
-                value={this.state.randomHigh}
-                onChange={e =>
-                  this.handleUpdateForm("randomHigh", e.target.value)
-                }
-              />
-            </Grid>
-          </Grid>
-
-          <Typography variant="h5" style={{ marginTop: 32 }}>
-            {`Result received: ${this.state.resultReceived}`}
-          </Typography>
-
-          <Typography variant="h5" style={{ marginTop: 32 }}>
-            {`Result: ${this.state.result}`}
-          </Typography>
-
-          <Grid container style={{ marginTop: 32 }}>
-            <Grid item xs>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => this.handleResetResult()}
-              >
-                Reset Result
-              </Button>
-            </Grid>
-            <Grid item xs>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => this.handleRequestResult()}
-              >
-                Request Result
-              </Button>
-            </Grid>
-          </Grid>
+        ))}
         </div>
-      </ThemeProvider>
+        <div>
+
+                <p>{this.state.response}</p>
+                <form onSubmit={this.handleSubmit}>
+                  <p>
+                    <strong>Post to Server:</strong>
+                  </p>
+                  <input
+                    type="text"
+                    value={this.state.post}
+                    onChange={e => this.setState({ post: e.target.value })}
+                  />
+                  <button type="submit">Submit</button>
+                </form>
+                <p>{this.state.responseToPost}</p>
+        </div>
+      </div>
     );
   }
 }
+
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/another-page/">Another Page</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/another-page/" component={AnotherPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
