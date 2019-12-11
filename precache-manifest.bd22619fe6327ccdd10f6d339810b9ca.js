412b532691a7b2c7d97ee1d9bb64f41e5b83250d
Miko Aro <arom@us.ibm.com@mikos-mbp.attlocal.net>
2019-12-11 12:03:33 -0600

    Updates

---
+++ precache-manifest.bd22619fe6327ccdd10f6d339810b9ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7b433a7f99fb379edb368191fb5dbb",
+    "url": "/cryptotracker/index.html"
+  },
+  {
+    "revision": "30d0a32bee3dffcd041f",
+    "url": "/cryptotracker/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "664c21a35655197fde62",
+    "url": "/cryptotracker/static/js/2.0a0c161f.chunk.js"
+  },
+  {
+    "revision": "30d0a32bee3dffcd041f",
+    "url": "/cryptotracker/static/js/main.99c143d4.chunk.js"
+  },
+  {
+    "revision": "f4bc2431fa4bf3c1862b",
+    "url": "/cryptotracker/static/js/runtime-main.f19cb816.js"
+  }
+]);
